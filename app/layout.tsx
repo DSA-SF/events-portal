6a79c53047892b6cf2b842d76f92f5f8bdea1da9
Eddy Hernandez <eddy.d.hernandez@gmail.com>
2023-11-24 12:22:44 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,8 @@ export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
-  pageProps
 }: {
   children: React.ReactNode,
-  pageProps: any
 }) {
   return (
       <html lang="en">
